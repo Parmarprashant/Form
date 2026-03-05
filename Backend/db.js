0db17349c47ee277b6ad7fd95dba1a9677f2f618
Prashant Parmar <prashantparmar9919@gmail.com>
2026-03-06 01:11:58 +0530

    added the .env file

--- Backend/db.js
+++ Backend/db.js
@@ -1,30 +1,14 @@
-// const mongoose = require("mongoose");
-
-// const mongoURL = require('./')
-// mongoose.connect(mongoURL);
-
-// const db  = mongoose.connection;
-
-
-// db.on("connected", ()=>{
-//     console.log("database is connected");
-// })
-
-// db.on("disconnect", ()=>{
-//     console.log("database is disconnected");
-// })
-
-// db.on("error", (err)=>{
-//     console.log("Error Occurs", err)
-// })
-
-// module.exports = db;
-
-
-
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Database connected"))
-.catch((err) => console.log(err));
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI)
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((err) => {
+    console.log("MongoDB connection error:", err);
+});
+
+module.exports = mongoose;
